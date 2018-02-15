9a8c9c0e49db90761af1de0b582a5c89e386c3b9
Victor Pinto <victor.pinto@olxbr.com>
2018-02-15 17:52:50 -0200

    debounce search term input

--- youtube-video-player/src/index.js
+++ youtube-video-player/src/index.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
+import _ from 'lodash';
+
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
@@ -30,9 +32,10 @@ class App extends Component {
     }
 
     render() {
+        const videoSearch = _.debounce(term => this.videoSearch(term), 300);
         return (
             <div>
-                <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+                <SearchBar onSearchTermChange={videoSearch} />
                 <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList videos={this.state.videos} onVideoSelect={selectedVideo => this.setState({selectedVideo})}
                 />
